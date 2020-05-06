9835bfc2fadc3986f9c29d63ce4a665fcf7aaf99
Remy Vuong <remy.vuong@davidson.fr>
2020-05-06 10:50:46 +0200

    Add IMAP credentials as env vars

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
 extern crate imap;
 extern crate native_tls;
 
+use std::env;
+
 fn main() {
     // To connect to the gmail IMAP server with this you will need to allow unsecure apps access.
     // See: https://support.google.com/accounts/answer/6010255?hl=en
     // Look at the gmail_oauth2.rs example on how to connect to a gmail server securely.
-    let message = fetch_inbox_top().unwrap();
+    fetch_inbox_top().unwrap();
 }
 
 fn fetch_inbox_top() -> imap::error::Result<Option<String>> {
-    let domain = "host";
+    let imap_host = env::var("IMAP_HOST").unwrap();
+    let imap_username = env::var("IMAP_USERNAME").unwrap();
+    let imap_password = env::var("IMAP_PASSWORD").unwrap();
+    let imap_port: u16 = env::var("IMAP_PORT").unwrap()
+        .to_string()
+        .parse()
+        .unwrap();
+
+    // let domain: &str = "imap.free.fr";
+    let domain: &str = imap_host.as_str();
+
     let tls = native_tls::TlsConnector::builder().build().unwrap();
 
     // we pass in the domain twice to check that the server's TLS
     // certificate is valid for the domain we're connecting to.
-    let client = imap::connect((domain, 993), domain, &tls).unwrap();
+    println!("Connecting to {:?}...", imap_host);
+    let client = imap::connect(
+        (domain, imap_port),
+        domain,
+        &tls,
+    ).unwrap();
+    println!("Connected: OK");
 
     // the client we have here is unauthenticated.
     // to do anything useful with the e-mails, we need to log in
+    println!("Authenticating as {:?}...", imap_username);
     let mut imap_session = client
-        .login("username", "password")
+        .login(imap_username.as_str(), imap_password.as_str())
         .map_err(|e| e.0)?;
+    println!("Authentication: OK");
 
     // we want to fetch the first email in the INBOX mailbox
+    println!("Selecting INBOX...");
     imap_session.select("INBOX")?;
+    println!("INBOX selected: OK");
 
     // fetch message number 1 in this mailbox, along with its RFC822 field.
     // RFC 822 dictates the format of the body of e-mails
