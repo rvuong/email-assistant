15eeedc5113522f522fdb78e5592a2a60db1b130
Remy Vuong <remy.vuong@davidson.fr>
2020-06-23 11:37:59 +0200

    Added a mock from the API Profiling in order to extract data

--- src/api/cooptation.rs
+++ src/api/cooptation.rs
@@ -15,10 +15,10 @@ use std::env;
  * POST data should be set dynamically
  */
 pub fn add(email: &Email, user_id: u32, token: String, url: &str) -> Result<(), Error> {
-    let _client = reqwest::blocking::Client::new();
+    let client = reqwest::blocking::Client::new();
 
     // POST a cooptation
-    let _url = format!("{}api/test/cooptation", url);
+    let url = format!("{}api/test/cooptation", url);
     let mut headers = HeaderMap::new();
     let authorization_value = format!("Bearer {}", token);
     headers.insert(AUTHORIZATION, authorization_value.parse().unwrap());
@@ -27,37 +27,31 @@ pub fn add(email: &Email, user_id: u32, token: String, url: &str) -> Result<(), 
     let root_path = env::current_dir().unwrap();
     let attachment_path = format!("{}/{}", root_path.display(), email.attachment);
 
-    let _branch = profiling::get(email, "branch").unwrap();
-    let _firstname = profiling::get(email, "firstname").unwrap();
-    let _lastname = profiling::get(email, "lastname").unwrap();
-    let _email = profiling::get(email, "email").unwrap();
-    let _relation = profiling::get(email, "relation").unwrap();
-    let _phone = profiling::get(email, "phone").unwrap();
+    let branch = profiling::get(email, "branch").unwrap();
+    let firstname = profiling::get(email, "firstname").unwrap();
+    let lastname = profiling::get(email, "lastname").unwrap();
+    let email_address = profiling::get(email, "email").unwrap();
+    let relation = profiling::get(email, "relation").unwrap();
+    let phone = profiling::get(email, "phone").unwrap();
 
     let form = multipart::Form::new()
-        .text("branch", _branch) // TODO Recommended branch IDs (","-separated list)
-        .text("firstname", _firstname) // TODO Candidate firstname, should be determined by Profiling
-        .text("lastname", _lastname) // TODO Candidate lastname, should be determined by Profiling
-        .text("email", _email) // TODO Candidate email, should be determined by Profiling
-        .text("relation", _relation) // TODO Either text/plain or html content of the input email
-        .text("phone", _phone) // TODO Candidate phone number, should be determined by Profiling
+        .text("branch", branch) // TODO Recommended branch IDs (","-separated list)
+        .text("firstname", firstname) // TODO Candidate firstname, should be determined by Profiling
+        .text("lastname", lastname) // TODO Candidate lastname, should be determined by Profiling
+        .text("email", email_address) // TODO Candidate email, should be determined by Profiling
+        .text("relation", relation) // TODO Either text/plain or html content of the input email
+        .text("phone", phone) // TODO Candidate phone number, should be determined by Profiling
         .text("userid", user_id)
         .file("cv", attachment_path)
         .unwrap();
 
-    let debug: bool = env::var("DEBUG")
-        .expect("Missing or invalid env var: DEBUG")
-        .parse()
+    let _res = client
+        .post(url.as_str())
+        .headers(headers)
+        .multipart(form)
+        .send()
         .unwrap();
-
-    if !debug {
-        let _res = _client
-            .post(_url.as_str())
-            .headers(headers)
-            .multipart(form)
-            .send()
-            .unwrap();
-    }
+    // TODO Assert result status() and manage potential errors
 
     Ok(())
 }
--- src/api/profiling.rs
+++ src/api/profiling.rs
@@ -1,4 +1,6 @@
 use crate::service::inbox::Email;
+use regex::Regex;
+use reqwest::header::CONTENT_TYPE;
 use std::io::Error;
 
 /**
@@ -10,7 +12,7 @@ pub fn get(_email: &Email, key: &str) -> Result<String, Error> {
         "firstname" => String::from("John"),
         "lastname" => String::from("Doe"),
         "email" => String::from("me@domain.tld"),
-        "relation" => String::from("--Relation"),
+        "relation" => get_body_text(_email).unwrap(),
         "phone" => String::from("01 02 03 04 05"),
         _ => {
             warn!("Invalid parameter name required: {}", key);
@@ -20,4 +22,34 @@ pub fn get(_email: &Email, key: &str) -> Result<String, Error> {
     };
 
     Ok(result)
+}
+
+/**
+ * @todo
+ *
+ * Returns the text/plain or text/html message body if any was found.
+ * Should be replaced by another method with more consistency.
+ */
+fn get_body_text(email: &Email) -> Result<String, Error> {
+    for item in email.body.iter().next() {
+        if item.is_attachment {
+            continue;
+        }
+
+        let headers_regex = Regex::new(r"(?m)^([a-zA-Z-]+): ([^\r]*)\r?$").unwrap();
+        for row in headers_regex.captures_iter(item.header.as_str()) {
+            if (&row[1]).to_ascii_lowercase().eq(CONTENT_TYPE.as_str()) {
+                // If the Content-Type is "text/plain" or "text/html"
+                if (&row[2]).contains("text/plain") {
+                    return Ok(item.content.to_string());
+                } else if (&row[2]).contains("multipart/alternative") {
+                    return Ok(
+                        "// TODO text/html message should be converted into text/plain".to_string(),
+                    );
+                }
+            }
+        }
+    }
+
+    return Ok("// TODO Message not found".to_string());
 }
